6839005f6bd4fc5bcd9cf21f72fd9c1090a2fa48
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-12-10 20:51:11 +0530

    Update console.py

--- DIL/console.py
+++ DIL/console.py
@@ -30,14 +30,14 @@ if os.path.exists("Internal"):
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "6541753178:AAGMpOcmo8KvLQ7yviurPAiPSu7BOgHlnlA")
-STRING_SESSION = getenv("STRING_SESSION", "BQGt9tsAlUezTyE3NXPzgyhZ58zkS8IcG2bdCq2IM43OBYqIWQe2FvASn0w-spivpvcLyjvxFhKQWdCL_gcK_XTu_41izKR21kd48DpRKt36AaHdxdf1RB6P54MhVWNMCaGvAwqdU9UMajcdl-j95zPZ42gOvaehq-MEI-1gUeKcd_fpSis5IO4MiPsJpQTtdphra9SDVqd1afc5fneiczrIQ3MHX21EZVT3-HEc2o0NumIpYjuTKmaVGTShMPtRaJHkos-f3mudmUQTvD8xmqCrrctYJglEQr3zxektJ0DYYcjfj_wNYwP_CmgQuMO98010gbbSmOF1WxKZMpCly0teia2YBwAAAAFC8M17AA")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Chiku12:Chiku12@arman.wsumgkn.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001963452122"))
 
 
 
-SESSION_STRING = getenv("SESSION_STRING", "BQGt9tsANPWXfgm6seuXcc3Vwj-r6uhrPIjL3dToZI3lyMAJ8W8TKHd4dw-D88q-D5dgdHQJ4TtgIvqAeedLzISgwShIITqWfzRTHB-2g2Q2PS8sFW1AGgveVjOqkmHpnB2Se6H0uaicTrntpUo-6lfvClazwKejA_Us-OKnEi0eXl7xL_MYOk8QK8XNfOMz-5cDqhiDKVpqpM68ojxYoHh3Tt5Ou8PqOYzn2A2gjLjYX1KrwHvupFxZ-KTWQhNshehU7DXwOlEUfIDUSXfvJHyGFPYPg-cgvkFeWAiRncnwO96LTvOAMMjHlKE0IPU1EiYDSL_lYKEtue4GWKLwHrHcxNfKyQAAAAFC8M17AA")
+SESSION_STRING = getenv("SESSION_STRING", "")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
 
 
